7e255e962fed0f18c2e21fb9738abbd4441d7514
Blaze-Casano <101803254+FireflyFl@users.noreply.github.com>
2022-07-20 11:31:10 +0900

    Build

---
+++ src/main/java/com/teamapp/travelsite/TravelsiteApplication.java
@@ -0,0 +1,13 @@
+package com.teamapp.travelsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelsiteApplication.class, args);
+	}
+
+}
