c713719ba6b2593361129384694a812e329ccdd8
Blaze-F <101803254+Blaze-F@users.noreply.github.com>
2022-07-30 23:24:09 +0900

    making local login

---
+++ backend/travelsite/src/main/java/com/teamapp/travelsite/Security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.teamapp.travelsite.Security;
+
+
+
+import com.teamapp.travelsite.Exception.ResourceNotFoundException;
+import com.teamapp.travelsite.User.User;
+import com.teamapp.travelsite.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
