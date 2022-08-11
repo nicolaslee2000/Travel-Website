f06b8a5c3a59200169a223fcdc1807b58b21101e
Blaze-F <101803254+Blaze-F@users.noreply.github.com>
2022-08-11 17:10:44 +0900

    Whyyyyyyy

--- frontend/final_project/src/pages/mainpage/Mainpage.js
+++ frontend/final_project/src/pages/mainpage/Mainpage.js
@@ -1,42 +1,23 @@
-import { Button } from "@mui/material";
-import { Container } from "@mui/system";
-import React from "react";
-import Layout from "../../components/othercomponent/Layout";
-import MainSearchPrac02 from "../../components/mainpagecomponent/MainSearchPrac02";
-import "./mainpage.css";
-//import Log_JoinPage from '../registerpage/RegisterPage';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import './mainPage.css';
+import MainSearch from '../../components/mainpagecomponent/MainSearch';
+import Header from '../../components/othercomponent/Header';
+import Layout from './../../components/othercomponent/Layout';
+import MainSearchPrac02 from './../../components/mainpagecomponent/MainSearchPrac02';
 
-const main = () => {
-    return (
-        <>
-            <Layout>
-                <div className="wrap">
-                    <div className="container">
-                        <MainSearchPrac02 />
-                    </div>
-                </div>
-            </Layout>
-        </>
-
-        // <Layout>
-        //   <Container maxWidth='sm'>
-        //     <div className='wrap'>
-        //       <div className='container'>
-        //         <Button variant='contained' href='/register'>
-        //           회원가입
-        //         </Button>
-        //         <Button variant='contained' href='/login'>
-        //           로그인
-        //         </Button>
-        //         <main>
-        //           <h1>메인페이지입니다.</h1>
-        //         </main>
-        //       </div>
-        //     </div>
-        //   </Container>
-        // </Layout>
-    );
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <MainSearchPrac02 />
+    </>
+  );
+  // return (
+  //   <>
+  //     <Layout children={<MainSearch />} />
+  //     {/* <Layout component={<MainSearchPrac02 />} /> */}
+  //   </>
+  // );
 };
 
-export default main;
+export default MainPage;
