513d278314ba48f51b84e3b53b39ff995da655e9
Blaze-F <101803254+Blaze-F@users.noreply.github.com>
2022-08-12 20:07:25 +0900

    Update MainPage.js

--- frontend/final_project/src/pages/mainpage/MainPage.js
+++ frontend/final_project/src/pages/mainpage/MainPage.js
@@ -5,7 +5,7 @@ import Header from '../../components/othercomponent/Header';
 import Layout from './../../components/othercomponent/Layout';
 import MainSearchPrac02 from './../../components/mainpagecomponent/MainSearchPrac02';
 
-const main = () => {
+const MainPage = () => {
   return (
     <>
       <div className='wrap'>
@@ -14,6 +14,24 @@ const main = () => {
         </div>
       </div>
     </>
+
+    // <Layout>
+    //   <Container maxWidth='sm'>
+    //     <div className='wrap'>
+    //       <div className='container'>
+    //         <Button variant='contained' href='/register'>
+    //           회원가입
+    //         </Button>
+    //         <Button variant='contained' href='/login'>
+    //           로그인
+    //         </Button>
+    //         <main>
+    //           <h1>메인페이지입니다.</h1>
+    //         </main>
+    //       </div>
+    //     </div>
+    //   </Container>
+    // </Layout>
   );
 };
 
