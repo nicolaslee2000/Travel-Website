a18112f04294a59a0259f169aa6a5ef38dc76ef8
Shon-Sang <dhsrbtka@naver.com>
2022-08-12 11:39:28 +0900

    axiosError searchNullError

---
+++ frontend/final_project/src/components/searchresultcomponent/NoSearchResultItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+const NoSearchResultItem = () => {
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        width: '1000px',
+        height: '900px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'mistyrose',
+      }}
+    >
+      <CardContent
+        sx={{ width: '500px', display: 'flex', justifyContent: 'center' }}
+      >
+        <Box>
+          <Typography variant='h4'>검색결과가 없습니다.</Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default NoSearchResultItem;
