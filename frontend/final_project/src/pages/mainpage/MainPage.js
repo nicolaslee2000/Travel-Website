cc1a13ad88a8bdf2a40e473dcb0da2cdb7afd099
Blaze-F <101803254+Blaze-F@users.noreply.github.com>
2022-08-12 19:55:09 +0900

    Update MainPage.js

--- frontend/final_project/src/pages/mainpage/MainPage.js
+++ frontend/final_project/src/pages/mainpage/MainPage.js
@@ -1,23 +1,22 @@
+import { Button } from '@mui/material';
+import { Container } from '@mui/system';
 import React from 'react';
-import './mainPage.css';
-import MainSearch from '../../components/mainpagecomponent/MainSearch';
-import Header from '../../components/othercomponent/Header';
-import Layout from './../../components/othercomponent/Layout';
-import MainSearchPrac02 from './../../components/mainpagecomponent/MainSearchPrac02';
+import Layout from '../../components/othercomponent/Layout';
+import MainSearchPrac02 from '../../components/mainpagecomponent/MainSearchPrac02';
+import './mainpage.css';
+//import Log_JoinPage from '../registerpage/RegisterPage';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const MainPage = () => {
+const main = () => {
   return (
     <>
-      <Header />
-      <MainSearchPrac02 />
+      <div className='wrap'>
+        <div className='container'>
+          <MainSearchPrac02 />
+        </div>
+      </div>
     </>
   );
-  // return (
-  //   <>
-  //     <Layout children={<MainSearch />} />
-  //     {/* <Layout component={<MainSearchPrac02 />} /> */}
-  //   </>
-  // );
 };
 
-export default MainPage;
+export default main;
