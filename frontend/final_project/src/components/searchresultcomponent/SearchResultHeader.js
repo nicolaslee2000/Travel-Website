e246e53503efc5b776d1d753276fe3f1b31d555d
Shon-Sang <dhsrbtka@naver.com>
2022-08-16 16:51:21 +0900

    update03

--- frontend/final_project/src/components/searchresultcomponent/SearchResultHeader.js
+++ frontend/final_project/src/components/searchresultcomponent/SearchResultHeader.js
@@ -1,7 +1,7 @@
-import React from "react";
-import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
-import "./searchResultHeader.css";
-import { useSelector } from "react-redux";
+import React from 'react';
+import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
+import './searchResultHeader.css';
+import { useSelector } from 'react-redux';
 
 const Searchresultheader = () => {
   //const info = props.info;
@@ -18,22 +18,22 @@ const Searchresultheader = () => {
 
   return (
     <>
-      <div className="headerContainer">
-        <div className="point">
-          <div className="startPoint">
+      <div className='headerContainer'>
+        <div className='point'>
+          <div className='startPoint'>
             <div>출발지</div>
             {/* <div>{info.start}</div> */}
             <div>{inputSearch.start.country}</div>
           </div>
-          <div className="endPoint">
+          <div className='endPoint'>
             <div>도착지</div>
             {/* <div>{info.end}</div> */}
             <div>{inputSearch.end.country}</div>
           </div>
         </div>
 
-        <div className="timePoint">
-          <div className="startPoint">
+        <div className='timePoint'>
+          <div className='startPoint'>
             <div>출발날짜</div>
             <div>{inputSearch.departDate}</div>
             {/* <div>{inputSearch.departureDate}</div> */}
@@ -41,7 +41,7 @@ const Searchresultheader = () => {
             {/* <div>20xx/xx/xx</div> */}
             {/* <div>{searData3.flightOfferSearch[0].lastTicketingDate}</div> */}
           </div>
-          <div className="endPoint">
+          <div className='endPoint'>
             <div>도착날짜</div>
             <div>{inputSearch.returnDate}</div>
             {/* <div>20xx/xx/xx</div> */}
