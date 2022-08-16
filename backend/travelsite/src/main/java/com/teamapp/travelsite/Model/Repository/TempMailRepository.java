1257492c8c2c3c34496d217cfb182f620b8752b3
eunjuny <joon2di@gmail.com>
2022-08-16 15:19:41 +0900

    Update TempMailRepository.java

--- backend/travelsite/src/main/java/com/teamapp/travelsite/Model/Repository/TempMailRepository.java
+++ backend/travelsite/src/main/java/com/teamapp/travelsite/Model/Repository/TempMailRepository.java
@@ -11,29 +11,12 @@ import com.teamapp.travelsite.Model.Entity.TempMail;
 
 @Repository
 public interface TempMailRepository  extends JpaRepository<TempMail,Long> {
-//	
-////    @Query(value = "select id from USER_TABLE where id = : id", nativeQuery = true)
-////    List<TempMail> searchParamRepo(@Param("id") String id);
-////
-////    Optional<TempMail> findByname(String username);
-//
-//    @Query(value =
-//            "SELECT * FROM TEMPMAIL_TABLE" +
-//                    "ORDER BY email"
-//            ,nativeQuery = true)
-//    List<TempMail> searchByEmail(@Param("email") String email);
-//
 
-//
-//    Boolean existsByEmail(String email);
-	
     Optional<TempMail> findByEmail(String email);
     
     //인증여부 검사
     @Query(value = "select emailauth from tempmail_table WHERE email = :email", nativeQuery = true)
     String searchEmailAuth(@Param("email") String email);
 
-    @Query(value = "DELETE FROM TEMPMAIL_TABLE WHERE EMAIL = :email", nativeQuery = true)
-    void deleteTempMail(@Param("email") String email);
 
 }
