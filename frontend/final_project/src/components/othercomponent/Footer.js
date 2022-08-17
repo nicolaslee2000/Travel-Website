a2f38319cb83d70d061e1821d9a270fffc859dcc
yooxx <hey2224mj@gmail.com>
2022-08-17 09:55:32 +0900

    Footer finish

--- frontend/final_project/src/components/othercomponent/Footer.js
+++ frontend/final_project/src/components/othercomponent/Footer.js
@@ -17,36 +17,71 @@ const Footer = () => {
     >
       <Grid container spacing={5}>
         {/* <Grid item xs={2}></Grid> */}
-        <Grid item xs={2}>
-        </Grid>
+        <Grid item xs={2}></Grid>
         <Grid item xs={8}>
           <div className='footer-container'>
             <h4>Travel Service</h4>
             <ul className='footer-list'>
               <li>
-              <Link underline="hover" color="inherit" href="/" target='_blank' fontSize="small">
-                손상기
-              </Link>
-              <span className='text_dot'>&nbsp;·&nbsp;</span>
-              <Link underline="hover" color="inherit" href="https://github.com/eunjuny" target='_blank' fontSize="small">
-              엽은준
-              </Link>
-              <span className='text_dot'>&nbsp;·&nbsp;</span>
-              <Link underline="hover" color="inherit" href="https://github.com/yooxx" target='_blank' fontSize="small">
-              유민정
-              </Link>
-              <span className='text_dot'>&nbsp;·&nbsp;</span>
-              <Link underline="hover" color="inherit" href="https://github.com/cupmilk" target='_blank' fontSize="small">
-              유정현
-              </Link>
-              <span className='text_dot'>&nbsp;·&nbsp;</span>
-              <Link underline="hover" color="inherit" href="https://github.com/nicolaslee2000" target='_blank' fontSize="small">
-              이건영
-              </Link>
-              <span className='text_dot'>&nbsp;·&nbsp;</span>
-              <Link underline="hover" color="inherit" href="https://github.com/Blaze-F" target='_blank' fontSize="small">
-              황준혁
-              </Link>  
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/Shon-Sang'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  손상기
+                </Link>
+                <span className='text_dot'>&nbsp;·&nbsp;</span>
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/eunjuny'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  엽은준
+                </Link>
+                <span className='text_dot'>&nbsp;·&nbsp;</span>
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/yooxx'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  유민정
+                </Link>
+                <span className='text_dot'>&nbsp;·&nbsp;</span>
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/cupmilk'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  유정현
+                </Link>
+                <span className='text_dot'>&nbsp;·&nbsp;</span>
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/nicolaslee2000'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  이건영
+                </Link>
+                <span className='text_dot'>&nbsp;·&nbsp;</span>
+                <Link
+                  underline='hover'
+                  color='inherit'
+                  href='https://github.com/Blaze-F'
+                  target='_blank'
+                  fontSize='small'
+                >
+                  황준혁
+                </Link>
               </li>
               <li>
                 <div className='footer-icon'>
