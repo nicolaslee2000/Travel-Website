33cf0c8cdcd3152123e4551b84324f7d2944c1ec
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-08-21 15:27:54 +0900

    UPDATE

--- backend/travelsite/src/main/java/com/teamapp/travelsite/Controller/UserController.java
+++ backend/travelsite/src/main/java/com/teamapp/travelsite/Controller/UserController.java
@@ -1,17 +1,21 @@
 package com.teamapp.travelsite.Controller;
 
 
+import com.teamapp.travelsite.Model.DTOs.UserDTO;
 import com.teamapp.travelsite.Model.Entity.User;
 import com.teamapp.travelsite.Model.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.JsonObject;
 import com.teamapp.travelsite.Exception.ResourceNotFoundException;
 import com.teamapp.travelsite.User.Security.CurrentUser;
 import com.teamapp.travelsite.User.Security.UserPrincipal;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,4 +29,37 @@ public class UserController {
     public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
         return userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
-}}
+}
+    
+    @PostMapping("/getUser")
+    public User getUserById(@RequestBody UserDTO dto) {
+    	System.out.println(dto);
+    	return userRepository.findByEmail(dto.getEmail()).orElseThrow();
+    }
+    
+    @PostMapping("/testingtesting")
+    public String getTesting(@RequestBody JsonObject test) {
+    	test.addProperty("hi", "hello");
+    	System.out.println(test.toString());
+    	return "hey it works";
+    }
+
+}
+
+class TestClass{
+	public TestClass() {
+		
+	}
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+this.name;
+	}
+}
--- frontend/final_project/src/pages/userDashboard/MyAccount/MyAccountPage.js
+++ frontend/final_project/src/pages/userDashboard/MyAccount/MyAccountPage.js
@@ -8,8 +8,25 @@ import {
     Button,
 } from "@mui/material";
 import GeneralInfo from "./GeneralInfo";
+import { useCookies } from "react-cookie";
 
 const MyAccountPage = () => {
+    const [user, setUser] = React.useState();
+    const [cookies, setCookie, removeCookie] = useCookies(["this_is_login"]);
+    const getUser = async (data, setState) => {
+        await fetch(`http://localhost:8090/getUser`, {
+            method: "post",
+            body: JSON.stringify({ email: cookies.this_is_login }),
+        }).then((response) => {
+            console.log(cookies);
+            console.log(response);
+        });
+    };
+
+    React.useEffect(() => {
+        getUser();
+    });
+
     return (
         <Box sx={{ width: 800 }}>
             <Container>
