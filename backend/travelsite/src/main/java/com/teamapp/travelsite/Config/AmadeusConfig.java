08f07e13b143906d3474e57dec87b851f94bcd2a
nicolaslee2000 <nicolaslee2000@gmail.com>
2022-09-02 17:47:22 +0900

    work?

--- backend/travelsite/src/main/java/com/teamapp/travelsite/Config/AmadeusConfig.java
+++ backend/travelsite/src/main/java/com/teamapp/travelsite/Config/AmadeusConfig.java
@@ -1,14 +1,16 @@
 package com.teamapp.travelsite.Config;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+import lombok.Data;
+
 @Configuration
-@PropertySource(value="amadeus.properties")
+@PropertySources(value = {@PropertySource("classpath:amadeus.properties")})
 @ConfigurationProperties(prefix = "travel-service")
 @Data
 public class AmadeusConfig {
--- frontend/final_project/src/ApiConnect/constants.js
+++ frontend/final_project/src/ApiConnect/constants.js
@@ -1,8 +1,11 @@
-export const BASE_URL = 'http://localhost:8090';
-export const ACCESS_TOKEN = 'accessToken';
+export const BASE_URL = "http://3.39.216.48:8090/travelsite-0.0.1-SNAPSHOT";
+export const ACCESS_TOKEN = "accessToken";
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
 
-export const GOOGLE_AUTH_URL = BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const FACEBOOK_AUTH_URL = BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GITHUB_AUTH_URL = BASE_URL + '/oauth2/authorize/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL =
+    BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
+export const FACEBOOK_AUTH_URL =
+    BASE_URL + "/oauth2/authorize/facebook?redirect_uri=" + OAUTH2_REDIRECT_URI;
+export const GITHUB_AUTH_URL =
+    BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
